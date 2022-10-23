f0184050f3873baa33c58525697c301fd386348d
kevin <kevin28042001@gmail.com>
2022-10-23 22:49:39 +0200

    Tutorial

---
+++ src/Basico/ShowMessage.java
@@ -0,0 +1,12 @@
+package Basico;
+
+import javax.swing.*;
+
+public class ShowMessage {
+
+    public static void main(String[] args) {
+        String nombre = "kevin";
+        JOptionPane.showMessageDialog(null, "Hola cabron: " + nombre);
+
+    }
+}
---
+++ src/Basico/Tutorial4.java
@@ -0,0 +1,17 @@
+package Basico;
+
+public class Tutorial4 {
+    public static void main(String[] args) {
+        //5+2=7
+
+        int num_uno = 5;
+        int num_dos = 2;
+        int resultado = 0;
+
+        resultado = num_uno + num_dos;
+
+        System.out.println("El resultado de los dos numeros es: " + resultado );
+
+
+    }
+}
